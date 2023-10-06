e3c2b91067ac7981873fa0dd64f4e88d9acc01dd
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-06 12:43:21 +0530

    updated

--- 03_MappingSpec/src/ExtensionMappingSpecServlet.java
+++ 03_MappingSpec/src/ExtensionMappingSpecServlet.java
@@ -1,2 +1,15 @@
-public class ExtensionMappingSpecServlet extends {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//Exact Mapping
+@WebServlet(urlPatterns = "/customer")
+public class ExtensionMappingSpecServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Exact Mapping");
+    }
 }
