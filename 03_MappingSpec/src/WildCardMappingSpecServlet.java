f54193588b81a3985fc135261f5b28f4df056587
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-06 12:46:14 +0530

    updated

--- 03_MappingSpec/src/WildCardMappingSpecServlet.java
+++ 03_MappingSpec/src/WildCardMappingSpecServlet.java
@@ -1,2 +1,12 @@
-public class WildCardMappingSpecServlet {
-}
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class WildCardMappingSpecServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            System.out.println("Wild Mapping Spec Invoke");
+        }
+    }
